fab2670f6e3f338f8efc7403e0b461b78366fdee
Artyom Vasiliev <artyom.v@namecheap.com>
2017-11-24 16:45:53 +0200

    add changes

---
+++ MVVMWPFDataTemplates/Interfaces/MyICommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVMWPFDataTemplates.Interfaces
+{
+
+    public class MyICommand : ICommand
+    {
+        readonly Action _targetExecuteMethod;
+        readonly Func<bool> _targetCanExecuteMethod;
+
+        public MyICommand(Action executeMethod)
+        {
+            _targetExecuteMethod = executeMethod;
+        }
+
+        public MyICommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            _targetExecuteMethod = executeMethod;
+            _targetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+
+            if (_targetCanExecuteMethod != null)
+            {
+                return _targetCanExecuteMethod();
+            }
+
+            if (_targetExecuteMethod != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // Beware - should use weak references if command instance lifetime is 
+        // longer than lifetime of UI objects that get hooked up to command
+ 
+        // Prism commands solve this in their implementation 
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        void ICommand.Execute(object parameter)
+        {
+            _targetExecuteMethod?.Invoke();
+        }
+    }
+}
---
+++ MVVMWPFDataTemplates/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MVVMFirstApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
