ac5ea7e6a8cc2ebda627e43ce6c8c7a4ae9b6e56
Artyom Vasiliev <artyom.v@namecheap.com>
2017-11-24 13:55:40 +0200

    add changes

---
+++ MVVMViewModelLocator/ViewModel/StudentViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using MVVMViewModelLocator.Model;
+
+namespace MVVMViewModelLocator.ViewModel
+{
+    public class StudentViewModel
+    {
+        public StudentViewModel()
+        {
+            LoadStudents();
+        }
+
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            var students = new ObservableCollection<Student>
+            {
+                new Student {FirstName = "Mark", LastName = "Allain"},
+                new Student {FirstName = "Allen", LastName = "Brown"},
+                new Student {FirstName = "Linda", LastName = "Hamerski"},
+                new Student {FirstName = "Linda", LastName = "Hamerski"}
+            };
+
+
+            Students = students;
+        }
+    }
+}
---
+++ MVVMViewModelLocator/Views/StudentView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace MVVMViewModelLocator.Views
+{
+    /// <summary>
+    /// Interaction logic for StudentView.xaml
+    /// </summary>
+    public partial class StudentView : UserControl
+    {
+        public StudentView()
+        {
+            InitializeComponent();
+            //DataContext = new ViewModel.StudentViewModel();
+        }
+    }
+}
